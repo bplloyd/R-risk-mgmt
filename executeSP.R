5b550a4350995768ffb89dc7246c6a529993b702
Bryan <bryan.lloyd@hatterasfunds.com>
2016-01-21 14:53:59 -0500

    -funcs

---
+++ executeSP.R
@@ -0,0 +1,10 @@
+executeSP <- function(procname, paramstring, db = "Hatteras_Sandbox_Tools"){
+  require("RODBC");
+  database = paste("database", db, sep = "=")
+  ch = paste("driver={SQL Server}", "server=HAT-SQL-01",database, "trusted_connection=true", sep = "; ")
+  ch = odbcDriverConnect(ch)
+  query <- paste("exec dbo.", procname, " ", paramstring);
+  res = sqlQuery(ch, query);
+  return(res);
+}
+result = sqlQuery(ch,"execute dbo.usp_GetPOFNAVS")
