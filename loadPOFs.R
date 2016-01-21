0c57d3a7a1125011f1d0205d2b0b3ce104488298
Bryan <bryan.lloyd@hatterasfunds.com>
2016-01-21 12:58:14 -0500

    -pof edits

--- loadPOFs.R
+++ loadPOFs.R
@@ -1,4 +1,4 @@
-loadPOFs = function(returns = TRUE){
+loadPOFs = function(){
   require(RODBC)
   require(magrittr)
   require(xts)
@@ -23,8 +23,20 @@ loadPOFs = function(returns = TRUE){
 			ORDER BY p.DateReported"
   pofs = sqlQuery(cn,qry)
   pofs= as.xts(pofs[,2:7], order.by = as.Date.character(pofs$DateReported))
-  if(returns == T){
-      pofs = pofs %>% CalculateReturns()
-  }
+  maxDate = as.Date.character(end(pofs), format = '%Y-%m-%d')
+
+  qry2 = "SELECT CAST(MAX(d.DateReported) AS date) 'DateReported' FROM Hatteras_SecurityDB.dbo.DailyTotalReturn AS d"
+  maxDateDTR = sqlQuery(cn,qry2)
+  maxDateDTR = as.Date.factor(maxDateDTR[[1]])
+  pofs = pofs %>% CalculateReturns()
+  if(maxDate!=maxDateDTR){
+    qry3 = "SELECT CAST(d.DateReported as date) 'DateReported', c.Symbol, d.OneDayReturn/100 'OneDayReturn' FROM Hatteras_SecurityDB.dbo.DailyTotalReturn AS d LEFT JOIN hatteras_Securitydb.dbo.fundclass AS c ON d.Fund_UID = c.Fund_UID and d.FundClass_UID = c.FundClass_UID WHERE d.DateReported > '"
+    qry3 = paste(qry3, maxDate, sep = ' ')
+    qry3 = paste(qry3,"') as A",  sep = ' ' )
+    qry3 = paste("SELECT p.* FROM (", qry3, sep = '')
+    qry3 = paste(qry3, "PIVOT(MAX(a.OneDayReturn) FOR a.SYMBOL IN ([ALPHX],[ALPIX],[HHSIX],[HFINX],[HLSIX], [HMFIX])) AS p ORDER BY p.DateReported", sep = ' ')
+    pofs2 = sqlQuery(cn, qry3)
+    pofs = rbind.xts(pofs, as.xts(pofs2[,names(pofs)], order.by = as.Date.character(pofs2$DateReported)))
+    }
   return(pofs)
 }
