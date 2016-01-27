8d8106b75871969b5a6c7a1e697b1fa574c2ff3a
Bryan <bryan.lloyd@hatterasfunds.com>
2016-01-27 17:18:45 -0500

    jan 27

--- printBounds.R
+++ printBounds.R
@@ -1,7 +1,7 @@
-printBounds = function(accounts){
+printBounds = function(accounts, p = 0.99, n = 126){
   print(paste("Name", "Date", "LowBound","UpBound", sep = "  |  "))
   for (i in 1:ncol(accounts)){
-    bounds = nVaRBounds(accounts[,i])
+    bounds = nVaRBounds(accounts[,i], p = p, n=n)
     print(paste(names(accounts)[i], 
                 end(bounds), 
                 bounds[end(bounds),1],
--- rollVaR.R
+++ rollVaR.R
@@ -1,15 +1,51 @@
-rollingWindows(uft, period = "12m", by = "1m")->wins
-myList = vector("list",length(wins$from))
-
-names(myList) = paste0("periodEnding_", seq_along(wins$to))
-
-for(i in 1:length(wins$from)){
-  myList[[i]] = VaR(R = na.omit(uft[paste(wins$from[i],"/", wins$to[i], sep = "")]), method = "gaussian", p = 0.99, portfolio_method = "component", weights = w)
+rollVaR  = function(assets, weights = NULL, p = 0.99, period = "12m", by = "1m", portfolio_method = "component"){
+  require(xts)
+  require(fPortfolio)
+  require(PortfolioAnalytics)
+  require(magrittr)
+  if(portfolio_method == "component"){
+      assets = na.omit(assets)
+  }
+  if(is.null(weights)){
+      weights = rep(1/ncol(assets), ncol(assets))
+  }
+  else{
+      weights = weights/sum(weights)
+  }
+  wins = rollingWindows(assets, period = period, by = by)
+  myList = vector("list",length(wins$from))
+  for (i in 1:length(myList)){
+    names(myList)[i] = paste0("periodEnding_",wins$to[i])
+  }
+  
+  for(i in 1:length(wins$from)){
+    if(portfolio_method == "component"){ 
+      myList[[i]] = VaR(R = assets[paste(wins$from[i],"/", wins$to[i], sep = "")], method = "gaussian", p = p, portfolio_method = portfolio_method, weights = weights)
+    }
+    if(portfolio_method == "single"){
+      if(ncol(assets[paste(wins$from[i],"/", wins$to[i], sep = ""),colSums(is.na(assets[paste(wins$from[i],"/", wins$to[i], sep = "")]))==0])>0)
+        myList[[i]] = VaR(R = assets[paste(wins$from[i],"/", wins$to[i], sep = ""),colSums(is.na(assets[paste(wins$from[i],"/", wins$to[i], sep = "")]))==0], method = "gaussian", p = p, portfolio_method = portfolio_method)
+    }
+  }
+  
+  if(portfolio_method == "component"){
+      result <- myList[[1]]$contribution %>% t() %>% as.data.frame()
+  }
+  if(portfolio_method == "single"){
+      result <- myList[[1]] %>% as.data.frame()
+  }
+  
+  for(i in 2:length(wins$from)){
+    if(portfolio_method == "component"){
+        result = result %>% rbind(myList[[i]]$contribution %>% t() %>% as.data.frame())
+    }
+    if(portfolio_method == "single"){
+      result = result %>% rbind(myList[[i]] %>% as.data.frame())
+    }
+  }
+  if(portfolio_method == "component"){
+      result$Total = apply(result, 1, sum)
+  }
+  return(xts(result, order.by = as.Date.timeDate(wins$to)))
 }
-
-alp.rollVaR <- myList[[1]]$contribution %>% t() %>% as.data.frame()
-for(i in 2:length(wins$from)){
-    alp.rollVaR = alp.rollVaR %>% rbind(myList[[i]]$contribution %>% t() %>% as.data.frame())
-}
-write.csv(alp.rollVaR, file = "alpRollVaR.csv")
 
