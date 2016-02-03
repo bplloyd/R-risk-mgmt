fd61914ac8218322f7f0d017a0e3758d88db382e
Bryan <bryan.lloyd@hatterasfunds.com>
2016-02-03 17:33:16 -0500

    2/3

---
+++ Functions/getSpSectors.R
@@ -0,0 +1,18 @@
+getSpSectors = function(inds=NULL){
+  require(xts)
+  require(PerformanceAnalytics)
+  if(is.null(inds))
+      inds = loadIndices()
+  
+  sp = inds[,grep("SPTR", names(ind))]
+  sp = sp[,-which(names(sp) %in% c("SPTR","SPTRSGX", "SPTRSVX", "SPTRTRNS"))]
+  
+  allNA = which(apply(sp, 1, function(x)all(is.na(x))))
+  if(length(allNA)!=0)
+      sp = sp[-allNA,]
+  
+  allNaN = which(apply(sp, 1, function(x)all(is.nan(x))))
+  if(length(allNaN)!=0)
+      sp = sp[-allNaN,]
+  return(CalculateReturns(sp))
+}
